b80cb3075b73f4f7e8dbcde8b5bf84501c887524
kotek <scpktk@gmail.com>
2024-06-15 01:19:47 +0900

    Add mocks prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       this is page
+      <Link href="/post-quiz">Go to post-quiz</Link>
     </div>
   );
 }
--- src/app/post-quiz/page.tsx
+++ src/app/post-quiz/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import Link from "next/link";
+
+export default function PostQuiz() {
   return (
-    <div>
-      this is post quiz page
+    <div className="text-center">
+      <div className="mt-24">問題を投稿</div>
+      <input placeholder="Input" className="border-gray-500 border-2 rounded"/>
+      <div>4つの回答を入力してください</div>
+      <div>
+        <input placeholder="Option 1" className="border-gray-500 border-2 rounded"/>
+        <input placeholder="Option 2" className="border-gray-500 border-2 rounded"/>
+        <input placeholder="Option 3" className="border-gray-500 border-2 rounded"/>
+        <input placeholder="Option 4" className="border-gray-500 border-2 rounded"/>
+      </div>
+      <Link href="/ans-quiz" className="border-2 border-gray-500 rounded bg-white hover:bg-slate-100 active:bg-slate-200 px-6 mt-6">OK</Link>
     </div>
   );
 }
