2b650b9b6e412645d0c949949420d2a3f5d582a1
kotek <scpktk@gmail.com>
2024-06-17 00:05:35 +0900

    Small refinement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
+"use client";
+
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="text-center">
-      <div>
-        this is page
-      </div>
+      <div>this is page</div>
       <div>
         <Link href="/waiting">Go to waiting page</Link>
       </div>
