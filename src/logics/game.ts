9973241d9458c65ef37e1da347042a9f2461ad7f
kotek <scpktk@gmail.com>
2024-06-19 08:24:14 +0900

    Add logics

---
+++ src/logics/game.ts
@@ -0,0 +1,37 @@
+/*
+ゲームの開始、終了を管理するクラス
+ゲーム開始時のイベントハンドラを登録することもできます。
+*/
+export class Game {
+  private static instance: Game;
+
+  public static getInstance() {
+    if (Game.instance == undefined) {
+      Game.instance = new Game([]);
+    }
+    return Game.instance;
+  }
+
+  // ゲーム開始時に実行されるイベントリスナ
+  private readonly gameStartEventListeners: (() => void)[];
+
+  private constructor(eventListeners: (() => void)[]) {
+    this.gameStartEventListeners = eventListeners;
+
+    // Firestoreのイベントリスナにstart()を登録
+  }
+
+  // イベントリスナを登録
+  public addGameStartEventListener(eventListener: () => void) {
+    this.gameStartEventListeners.push(eventListener);
+  }
+
+  // ゲームを開始
+  public start() {
+    "use client";
+    for (const eventListener of this.gameStartEventListeners) {
+      eventListener();
+    }
+    console.log("Game start");
+  }
+}
