857d91b7ec170788df8d2e70b8aa89d9a46c34be
tomizawakenshin <kenshin.8799@gmail.com>
2024-06-20 17:24:10 +0900

    コメントアウト整理した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,15 @@
 "use client";
 
 import TeamButtons from "@/components/TeamButtons";
-import TeamCard from "@/components/TeamCard";
 import { fetchTeamsCount } from "@/logics/CountTheNumberOfTeams";
 import { signInWithAnonymous } from "@/logics/SignInWithAnonymous";
-import { auth, db } from "@/logics/firebase";
-import { Team } from "@/logics/types/team";
-import { onAuthStateChanged, signInAnonymously } from "firebase/auth";
-import { collection, getDocs } from "firebase/firestore";
-import { useRouter } from "next/navigation";
+import { auth } from "@/logics/firebase";
 import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Home() {
   const [user] = useAuthState(auth);
   const [teamsCount, setTeamsCount] = useState(0);
-
-  const [teams, setTeams] = useState<Team[]>([
-    { name: "Team1", id: "1" },
-    { name: "Team2", id: "2" },
-    { name: "Team3", id: "3" },
-    { name: "Team4", id: "4" },
-  ]);
-
-  const router = useRouter();
-
-  const teamCards = teams.map((team) => <TeamCard key={team.id} router={router}>{team.name}</TeamCard>);
 
   useEffect(() => {
     const getTeamsCount = async () => {
@@ -37,9 +21,6 @@ export default function Home() {
     getTeamsCount();
   }, [])
 
-  // function gameStartButtonHandler() {
-  //   signInWithAnonymous
-  // }
 
   return (
     <div>
