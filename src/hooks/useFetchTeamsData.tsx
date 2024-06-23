a50241806ce0eb0c221781bd1703ea84a1abc9cb
ひろ <koki.hiro0723@gmail.com>
2024-06-23 19:41:05 +0900

    ✨ Resultページに渡された集計結果の表示

---
+++ src/hooks/useFetchTeamsData.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { fetchTeamById } from "@/logics/fetchTeams";
+
+interface TeamScore {
+  teamId: string;
+  matchRate: number;
+}
+
+interface TeamData {
+  name: string;
+  matchRate: number;
+}
+
+export const useFetchTeamsData = (scores: TeamScore[]) => {
+  const [teams, setTeams] = useState<TeamData[]>([]);
+
+  useEffect(() => {
+    const fetchTeamsData = async () => {
+      const teamsData = await Promise.all(
+        scores.map(async (score) => {
+          const team = await fetchTeamById(score.teamId);
+          return { name: team.name, matchRate: (score.matchRate || 0) * 100 };
+        })
+      );
+      setTeams(teamsData);
+    };
+
+    fetchTeamsData();
+  }, [scores]);
+
+  return teams;
+};
