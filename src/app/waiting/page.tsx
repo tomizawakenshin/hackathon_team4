7f9607fe234fd10ff1cabb06f0946f2c62f23a6d
ひろ <koki.hiro0723@gmail.com>
2024-06-24 02:47:25 +0900

    💄 待機画面のUIを作成

--- src/app/waiting/page.tsx
+++ src/app/waiting/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import sandwichPersonImage from "@/assets/images/person-on-sandwich.gif";
+import matchStickImage from "@/assets/images/match-stick.png";
+import { useEffect } from "react";
+import { useDisableScroll } from "@/hooks/useDisableScroll";
 import { handleStartGame } from "@/logics/FetchStartAPI";
 import { listenToGameStart } from "@/logics/MonitorGameStartFlag";
 import { goToPage } from "@/logics/server/goToPage";
-import { useEffect } from "react";
 
-export default function Waiting() {
+const WaitingPage: React.FC = () => {
+  useDisableScroll();
+
   useEffect(() => {
     const unsubscribe = listenToGameStart(() => {
       goToPage("/post");
@@ -21,11 +26,20 @@ export default function Waiting() {
   }, []);
 
   return (
-    <div className="text-center">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center bg-gray-300 text-white">
       <div className="flex justify-center">
-        <div>in チーム1</div>
+        <img src={sandwichPersonImage.src} alt="sandwich" className="w-64" />
       </div>
-      <div>開始までお待ち下さい……………………</div>
+      <h1 className="text-5xl font-bold my-8">スタンバイ...</h1>
+      <p className="text-3xl font-bold mb-1">暇つぶし</p>
+      <p className="text-xl font-semibold mb-4">
+        マッチ棒を2本動かして人にして
+      </p>
+      <div className="flex justify-center">
+        <img src={matchStickImage.src} alt="match" className="w-96" />
+      </div>
     </div>
   );
-}
+};
+
+export default WaitingPage;
