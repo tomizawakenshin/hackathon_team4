cc964c97a45cda7b322a794b87308c38d8ae7458
ひろ <koki.hiro0723@gmail.com>
2024-06-23 19:39:46 +0900

    ⚡️ 名称の変更

--- src/components/TeamRanking.tsx
+++ src/components/TeamRanking.tsx
@@ -2,32 +2,32 @@ import { getColorClass } from "@/utils/getColorClass";
 
 interface Team {
   name: string;
-  correctRate: number;
+  matchRate: number;
 }
 
 interface TeamRankingProps {
   teams: Team[];
-  maxCorrectRate: number;
+  maxMatchRate: number;
 }
 
-const TeamRanking: React.FC<TeamRankingProps> = ({ teams, maxCorrectRate }) => {
+const TeamRanking: React.FC<TeamRankingProps> = ({ teams, maxMatchRate }) => {
   return (
     <div className="flex flex-col gap-1 w-full border-l-8 border-black py-6">
       {teams.map((team, index) => (
         <div
           key={team.name}
           className="flex items-center mb-2"
-          style={{ width: `${team.correctRate}%` }}
+          style={{ width: `${team.matchRate}%` }}
         >
           <div
             className={`flex-1 py-2 px-4 font-bold text-white ${getColorClass(index)}`}
           >
             {team.name}
           </div>
-          {team.correctRate === maxCorrectRate && (
+          {team.matchRate === maxMatchRate && (
             <div
               className="absolute left-60 ml-2 text-yellow-300 font-mono font-bold text-2xl bg-transparent"
-              style={{ left: `${team.correctRate * 0.7}%` }}
+              style={{ left: `${team.matchRate * 0.7}%` }}
             >
               WIN
             </div>
