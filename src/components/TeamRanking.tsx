a2223058ae58ce72b4299a24274507fa67c022bc
ひろ <koki.hiro0723@gmail.com>
2024-06-26 04:00:13 +0900

    ✨ ResultPageにアニメーションを追加

--- src/components/TeamRanking.tsx
+++ src/components/TeamRanking.tsx
@@ -1,4 +1,6 @@
+import { useContainerVariants, useItemVariants } from "@/hooks/useAnimation";
 import { getColorClass } from "@/utils/getColorClass";
+import { motion } from "framer-motion";
 
 interface Team {
   name: string;
@@ -11,30 +13,47 @@ interface TeamRankingProps {
 }
 
 const TeamRanking: React.FC<TeamRankingProps> = ({ teams, maxMatchRate }) => {
+  const containerVariants = useContainerVariants();
+
   return (
-    <div className="flex flex-col gap-1 w-full border-l-8 border-black py-6">
-      {teams.map((team, index) => (
-        <div
-          key={team.name}
-          className="flex items-center mb-2"
-          style={{ width: `${team.matchRate}%` }}
-        >
-          <div
-            className={`flex-1 py-2 px-4 font-bold text-white ${getColorClass(index)}`}
+    <motion.div
+      className="flex flex-col gap-1 w-full border-l-8 border-black py-6"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      {teams.map((team, index) => {
+        const width = `${team.matchRate}%`;
+        const delay = (teams.length - 1 - index) * 0.2;
+        const itemVariants = useItemVariants(width);
+
+        return (
+          <motion.div
+            key={team.name}
+            className="flex items-center mb-2 relative"
+            initial="hidden"
+            animate="visible"
+            variants={itemVariants}
+            transition={{ duration: 1, delay }}
+            style={{ width }}
           >
-            {team.name}
-          </div>
-          {team.matchRate === maxMatchRate && (
             <div
-              className="absolute left-60 ml-2 text-yellow-300 font-mono font-bold text-2xl bg-transparent"
-              style={{ left: `${team.matchRate * 0.7}%` }}
+              className={`flex-1 py-2 px-4 font-bold text-white ${getColorClass(index)}`}
             >
-              WIN
+              {team.name}
             </div>
-          )}
-        </div>
-      ))}
-    </div>
+            {team.matchRate === maxMatchRate && (
+              <div
+                className="absolute text-yellow-300 font-mono font-bold text-2xl bg-transparent"
+                style={{ left: `calc(${team.matchRate * 0.7}% + 30px)` }}
+              >
+                WIN
+              </div>
+            )}
+          </motion.div>
+        );
+      })}
+    </motion.div>
   );
 };
 
