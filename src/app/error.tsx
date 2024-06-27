2df8b13e868e62ac89673a02bbd5c7afe30eae4e
kotek <scpktk@gmail.com>
2024-06-28 01:46:08 +0900

    ボタンにhoverとactiveを追加

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -19,7 +19,7 @@ export default function Error(props: Props) {
       <h2 className="mx-16 rounded border-2 border-red-400 bg-red-50 p-4 font-bold">エラーが発生しました😢</h2>
       <p className="mt-4 text-xs">{props.error.message}</p>
       <button
-        className="mt-16 rounded-full bg-red-300 px-6 py-2 font-bold text-white"
+        className="mt-16 rounded-full bg-red-300 px-6 py-2 font-bold text-white hover:bg-red-400 active:bg-red-500 transition"
         onClick={() => {
           props.reset();
         }}
--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -20,7 +20,7 @@ const SubmitButton: React.FC<SubmitButtonProps> = ({
         onClick={handleSubmit}
         disabled={isDisabled}
         className={`w-full max-w-36 px-4 py-2 text-2xl font-semibold text-white rounded-full ${
-          isDisabled ? "bg-gray-400" : "bg-red-300"
+          isDisabled ? "bg-gray-400" : "bg-red-300 hover:bg-red-400 active:bg-red-500"
         }`}
       >
         {buttonText}
