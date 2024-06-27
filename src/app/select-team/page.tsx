0f8fe7401a0cfb6b5908ff8629278e4fc69270fc
kotek <scpktk@gmail.com>
2024-06-28 02:58:30 +0900

    ホーム画面から自動で画面遷移しないように変更
    ゲーム開始を押すとユーザ登録されます

---
+++ src/app/select-team/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import SelectTeamPanel from "@/components/SelectTeam";
+
+export default function SelectTeam() {
+  return (
+    <div>
+      <SelectTeamPanel />
+    </div>
+  );
+}
