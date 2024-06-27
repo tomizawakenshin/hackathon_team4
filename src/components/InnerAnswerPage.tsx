f22938f180df65e4f2af50cbe2e0c53275bf3f72
tomizawakenshin <kenshin.8799@gmail.com>
2024-06-28 00:38:47 +0900

    answerページのレイアウトを修正しました

--- src/components/InnerAnswerPage.tsx
+++ src/components/InnerAnswerPage.tsx
@@ -18,6 +18,7 @@ import QuizOptions from "./QuizOptions";
 import SubmitButton from "./SubmitButton";
 import quizOptionsImage from "@/assets/images/quiz-options.png";
 import bgImage from "@/assets/images/bg-answerPage.jpg";
+import AnswerPage_TeamHeader from "./AnswerPage_TeamHeader";
 
 const InnerAnswerPage: React.FC = () => {
   const router = useRouter();
@@ -61,7 +62,7 @@ const InnerAnswerPage: React.FC = () => {
       className="flex min-h-screen flex-col items-center justify-center bg-green-300 text-white bg-cover bg-center"
       style={{ backgroundImage: `url(${bgImage.src})` }}
     >
-      <TeamHeader teamName={teamName} imageSrc={quizOptionsImage.src} />
+      <AnswerPage_TeamHeader teamName={"VolareOsaka"} imageSrc={quizOptionsImage.src} />
       <QuizQuestion question={quiz.question} />
       <QuizOptions
         options={quiz.options}
@@ -73,7 +74,6 @@ const InnerAnswerPage: React.FC = () => {
         selectedOption={selectedOption}
         buttonText="回答!"
       />
-      <div>{user?.uid}</div>
     </div>
   );
 };
